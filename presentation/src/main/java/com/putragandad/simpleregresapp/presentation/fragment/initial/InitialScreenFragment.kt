7effb269ae2f97a2519dd956b5ac9d9ed13d0d89
PutraGandaD <putraganda.dewata@gmail.com>
2024-08-14 02:33:06 +0700

    feat: Finished slicing all three screen design

--- presentation/src/main/java/com/putragandad/simpleregresapp/presentation/fragment/initial/InitialScreenFragment.kt
+++ presentation/src/main/java/com/putragandad/simpleregresapp/presentation/fragment/initial/InitialScreenFragment.kt
@@ -5,14 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.putragandad.simpleregresapp.presentation.R
+import com.putragandad.simpleregresapp.presentation.databinding.FragmentInitialScreenBinding
 
 class InitialScreenFragment : Fragment() {
+    private var _binding: FragmentInitialScreenBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_initial_screen, container, false)
+        _binding = FragmentInitialScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnNext.setOnClickListener {
+            findNavController().navigate(R.id.action_initialScreenFragment_to_secondScreenFragment)
+        }
     }
 }
